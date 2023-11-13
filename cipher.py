9bdcb765bd47e196957bbb5b181f5c610b1f31cf
Ting-Wen <129238267+u8202025@users.noreply.github.com>
2023-11-13 00:23:43 -0500

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,11 +1,25 @@
 # add your code here
-alphabets = 'abcdefghijklmnopqrstuvwxyz'
-def final_words(original_words, shift=5):
-    def shifted_letter(char):
-        if char in alphabets:
-            char_num = ord(char) - 97
-            char = chr((char_num + shift) % 26 + 97 )
-        return char
-    return ''.join(shifted_letter(char) for char in original_words)
-original_words = input('Please enter a senctence: ')
-print('The encrypted sentence is:', final_words(original_words, shift=5))
+def encrypt(sentence):
+    encrypted_sentence = ""
+    for char in sentence:
+        if char.isalpha():
+            # Encrypt only alphabetic characters
+            offset = 13  # You can choose a different offset for encryption
+            if char.islower():
+                encrypted_char = chr((ord(char) - ord('a') + offset) % 26 + ord('a'))
+            else:
+                encrypted_char = chr((ord(char) - ord('A') + offset) % 26 + ord('A'))
+            encrypted_sentence += encrypted_char
+        else:
+            # Keep non-alphabetic characters unchanged
+            encrypted_sentence += char
+    return encrypted_sentence
+
+# Get user input
+user_input = input("Please enter a sentence: ")
+
+# Encrypt the sentence
+encrypted_sentence = encrypt(user_input)
+
+# Display the result
+print("The encrypted sentence is:", encrypted_sentence)
